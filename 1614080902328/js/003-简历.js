8730d9ef5fc0556039cd950528fda76f2a7699ab
liangzhuoh <37769318+liangzhuoh@users.noreply.github.com>
2018-11-12 01:49:17 +0800

    Add files via upload

--- 1614080902328/js/003-简历.js
+++ 1614080902328/js/003-简历.js
@@ -1,13 +1,45 @@
 window.onload = function () {
-    var text = document.getElementById("text");
-    var btn = document.getElementById("subBtn");
-    btn.onclick = function () {
-        var content = text.value;
-        if(content=="") {
-            alert("内容为空，请继续输入");
-            return false;
-        }
-        else
-            this.submit();
-    }
+
+
+    // $(function(){
+    //     var $toast = $('#toast');
+    //     $('#showToast').on('click', function(){
+    //         if ($toast.css('display') != 'none') return;
+    //
+    //         $toast.fadeIn(100);
+    //         setTimeout(function () {
+    //             $toast.fadeOut(100);
+    //         }, 2000);
+    //     });
+    // });
+
+
+    $(function(){
+        var $iosDialog2 = $('#iosDialog2');
+        $('#iosDialog2').on('click', '.weui-dialog__btn', function(){
+            $(this).parents('.js_dialog').fadeOut(200);
+        });
+
+        $('#showToast').on('click', function(){
+            if(document.getElementById("text").value=="")
+            {$iosDialog2.fadeIn(200);
+            }
+            else
+            {
+                var $toast = $('#toast');
+                $('#showToast').on('click', function(){
+                    if ($toast.css('display') != 'none') return;
+
+                    $toast.fadeIn(100);
+                    setTimeout(function () {
+                        $toast.fadeOut(100);
+                    }, 2000);
+                });
+            }
+        });
+
+    });
+
+
+
 }
