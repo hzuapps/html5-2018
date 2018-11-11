bd40b0761cd987ffcc23d3141be5ec246ca4bb5b
BFFat <562804497@qq.com>
2018-11-11 22:42:10 +0800

    4

--- 1614080902222/form.js
+++ 1614080902222/form.js
@@ -7,28 +7,26 @@ window.onload = function(){  //在页面加载之后动态事件绑定
     //取出它的值
         // alert(emailObj.value);
         if (emailObj.value == "") {
-            alert("您还有输入登录邮箱，无法登录！");
-            if (e && e.preventDefault) { //现在是在W3C标准下执行
-                e.preventDefault(); //阻止浏览器的动作
-            }else{  //专门针对于IE浏览器的处理
-                window.event.returnValue= false;
-            }
-        }else{  //输入正确，理论上应该可以进行提交
-            alert(emailObj.value);
-
-            if (/^\w+@\w+\.\w+$/.test(emailObj.value)) {
-                this.submit(); //当前元素提交表单
-            }else{  //验证不通过
-                alert("请输入合法的EMAIL地址!");
-                if (e && e.preventDefault) { //现在是在W3C标准下执行
-                e.preventDefault(); //阻止浏览器的动作
-                }else{  //专门针对于IE浏览器的处理
-                    window.event.returnValue= false;
-                }
-            }
+      alert("您还没有输入留言内容！");
+      if (e && e.preventDefault) { //现在是在W3C标准下执行
+        e.preventDefault(); //阻止浏览器的动作
+      }else{  //专门针对于IE浏览器的处理
+        window.event.returnValue= false;
+      }
+    }else{  //输入正确，理论上应该可以进行提交
+      if(confirm("你要提交的内容是 "+emailObj.value)) {
+        this.submit(); //当前元素提交表单
+      }
+      else{
+        if (e && e.preventDefault) { //现在是在W3C标准下执行
+          e.preventDefault(); //阻止浏览器的动作
+        }else{  //专门针对于IE浏览器的处理
+          window.event.returnValue= false;
         }
-    },false);
+      }
+    }
+  },false);
 }
-function submit () {
-    return false;
+function submit() {
+  return false;
 }
