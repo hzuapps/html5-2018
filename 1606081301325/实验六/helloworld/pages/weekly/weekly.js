cc0ebb51b6b5a12f2254343870534d3f48f24a07
luohao <luohao>
2018-11-20 11:53:03 +0800

    #6 第六次实验

---
+++ 1606081301325/实验六/helloworld/pages/weekly/weekly.js
@@ -0,0 +1,13 @@
+Page({
+  data:{
+    thisWeeklMovie:{
+      name:"教父",
+      comment:"最精彩的剧本，最真实的黑帮电影。",
+      imagePath:"/images/_ipe2.jpg",
+      isHighlyRecommended: true
+    },
+    count:123,
+    score:61
+
+  }
+})
