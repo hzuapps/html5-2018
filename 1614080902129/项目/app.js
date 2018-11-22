23d18360b3f8a95bd36ab5811ac337cc06af90a4
bujingyu <645574339@qq.com>
2018-11-22 22:58:58 +0800

    #6 实验6

---
+++ 1614080902129/项目/app.js
@@ -0,0 +1,17 @@
+App({
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+
+  appData : {
+    userinfo : null,
+  }
+
+
+})
