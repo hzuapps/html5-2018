2269bc54eeffe19de1190c56ef448c345ac2932c
Your Name <you@example.com>
2018-11-24 23:25:52 +0800

    #6第6次实验

---
+++ 1614080902242/龙空推书/pages/rangings1/rangings1.js
@@ -0,0 +1,21 @@
+// pages/rangings1/rangings1.js
+Page({
+  data: {
+    listData: [
+      { "code": "01", "text": "全球高武", "type": "老鹰吃小鸡" },
+      { "code": "02", "text": "天道图书馆", "type": "横扫天涯" },
+      { "code": "03", "text": "诡秘之主", "type": "爱潜水的乌贼" },
+      { "code": "04", "text": "我有一座恐怖屋", "type": "我会修空调" },
+      { "code": "05", "text": "太初", "type": "高楼大厦" },
+      { "code": "06", "text": "咬火", "type": "这里有妖气" },
+      { "code": "07", "text": "民国谍影", "type": "寻青藤" },
+      { "code": "08", "text": "大王饶命", "type": "会说话的肘子" },
+      { "code": "09", "text": "明朝败家子", "type": "上山打老虎额" },
+      { "code": "10", "text": "道君", "type": "跃千愁" }
+    ]
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+
+})
