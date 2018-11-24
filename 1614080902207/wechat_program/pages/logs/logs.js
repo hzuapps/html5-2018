96dc8469bc6395eb470f46180339abf7bb09a908
Shinelon <Shinelon>
2018-11-25 00:47:53 +0800

    #6 实验六

---
+++ 1614080902207/wechat_program/pages/logs/logs.js
@@ -0,0 +1,72 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    date: "2016-09-01",
+    countDownDay: 0,
+    dataList:{}
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  onReady: function () {
+    var totalSecond = 1505540080 - Date.parse(new Date()) / 1000;
+
+    var interval = setInterval(function () {
+      // 秒数
+      var second = totalSecond;
+
+      // 天数位
+      var day = Math.floor(second / 3600 / 24);
+      var dayStr = day.toString();
+      if (dayStr.length == 1) dayStr = '0' + dayStr;
+
+      // 小时位
+      var hr = Math.floor((second - day * 3600 * 24) / 3600);
+      var hrStr = hr.toString();
+      if (hrStr.length == 1) hrStr = '0' + hrStr;
+
+      // 分钟位
+      var min = Math.floor((second - day * 3600 * 24 - hr * 3600) / 60);
+      var minStr = min.toString();
+      if (minStr.length == 1) minStr = '0' + minStr;
+
+      // 秒位
+      var sec = second - day * 3600 * 24 - hr * 3600 - min * 60;
+      var secStr = sec.toString();
+      if (secStr.length == 1) secStr = '0' + secStr;
+
+      this.setData({
+        countDownDay: dayStr,
+        countDownHour: hrStr,
+        countDownMinute: minStr,
+        countDownSecond: secStr,
+      });
+      totalSecond--;
+      if (totalSecond < 0) {
+        clearInterval(interval);
+        wx.showToast({
+          title: '活动已结束',
+        });
+        this.setData({
+          countDownDay: '00',
+          countDownHour: '00',
+          countDownMinute: '00',
+          countDownSecond: '00',
+        });
+      }
+    }.bind(this), 1000);
+  },
+
+  //cell事件处理函数
+  bindCellViewTap: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '../babyDetail/babyDetail?id=' + id
+    });
+  }
+
+})
