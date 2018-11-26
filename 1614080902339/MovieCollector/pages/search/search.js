b6e44514cc677cd188745c1b6d3436df7cdf1191
Strakly <594956580@qq.com>
2018-11-27 01:40:10 +0800

    #7 #874 MovieCollector

--- 1614080902339/MovieCollector/pages/search/search.js
+++ 1614080902339/MovieCollector/pages/search/search.js
@@ -1,66 +1,19 @@
-// pages/search/search.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    movieNu:null,
+    movieInfo:null,
+    line:'\n'
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  btnClick : function(){
+    var thispage = this;
+    app.getMovieInfo(this.data.movieNu,function(data){
+        thispage.setData({movieInfo:data})
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  input : function(e){
+    this.setData({ movieNu:e.detail.value})
   }
 })
