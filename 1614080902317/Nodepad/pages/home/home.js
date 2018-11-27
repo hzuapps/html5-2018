f1ecf6081880e5fa1eb9b246c2f5b314de2110d5
zhengjinbin <1033859407@qq.com>
2018-11-27 21:30:01 +0800

    #7 #841 实验7

--- 1614080902317/Nodepad/pages/home/home.js
+++ 1614080902317/Nodepad/pages/home/home.js
@@ -7,12 +7,42 @@ Page({
   data: {
 
   },
+  
+  formSubmit: function (e) {
+    var that = this;
+    var formData = e.detail.value.id; //获取表单所有name=id的值 
+    wx.request({
+      url: 'http://localhost/shiyan/select.php?name=' + formData,
+      data: formData,
+      header: { 'Content-Type': 'application/json' },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data == "") {
+          wx.showModal({
+            title: '提示',
+            content: '该美食不存在！',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+              } else if (res.cancel) {
+                console.log('用户点击取消')
+              }
+            }
+          })  
+        }
+        that.setData({
+          list_data: res.data,
+        })
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
--- 1614080902317/Nodepad/pages/index/index.js
+++ 1614080902317/Nodepad/pages/index/index.js
@@ -33,7 +33,20 @@ Page({
     })
   },
   onLoad: function () {
-   
+    var _this = this
+    var list_data
+    wx.request({
+      url: 'https://localhost/shiyan/data.php',//json数据地址
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        _this.setData({
+          list_data: res.data,
+        })
+      }
+    })    
   }
   
 })
