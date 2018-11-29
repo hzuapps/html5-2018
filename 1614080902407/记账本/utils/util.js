afea8433ae0601f257846ec71471a468518b68fb
Qiujialin <78973190@qq.com>
2018-11-29 19:55:34 +0800

    #6 第六次实验

---
+++ 1614080902407/记账本/utils/util.js
@@ -0,0 +1,25 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+const formatTimeday = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return year+"年"+month+"月"+day+"日"
+}
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime,
+  formatTimeday: formatTimeday
+}
