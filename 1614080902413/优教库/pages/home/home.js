85afb49f8d84d0a769f9dd2b708b76bd3f3cf331
zzhuangj <1151131005@qq.com>
2018-11-29 23:24:50 +0800

    #6 实验6

---
+++ 1614080902413/优教库/pages/home/home.js
@@ -0,0 +1,17 @@
+
+var app = getApp()
+Page({
+  jumpBtn1: function(options){
+    wx.navigateTo({
+      url: '../in_publish/in_publish',
+    })
+  },
+  jumpBtn2: function (options) {
+    wx.navigateTo({
+      url: '../in_search/in_search',
+    })
+  },
+
+
+
+})  
