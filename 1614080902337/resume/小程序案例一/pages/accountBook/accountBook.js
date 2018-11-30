d75c2c0799f1f3edc63b26c56ac7cdd353b504a9
DcXuhm <1019103752@qq.com>
2018-11-30 21:04:39 +0800

    #7 实验7

--- 1614080902337/resume/小程序案例一/pages/accountBook/accountBook.js
+++ 1614080902337/resume/小程序案例一/pages/accountBook/accountBook.js
@@ -1,1 +1,89 @@
-Page({})
+Page({
+
+  //初始化数据
+
+  data: {
+
+    array: ['家中', '学校', '公司', '野外'],
+
+    index: 0,
+
+    date: '2018-01-01',
+
+    time: '12:00',
+
+    allValue: ''
+
+  },
+
+  //表单提交按钮
+
+  formSubmit: function (e) {
+
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+
+    this.setData({
+
+      allValue: e.detail.value
+
+    })
+
+  },
+
+  //表单重置按钮
+
+  formReset: function (e) {
+
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+
+    this.setData({
+
+      allValue: ''
+
+    })
+
+  },
+
+  //---------------------与选择器相关的方法
+
+  //地区选择
+
+  bindPickerChange: function (e) {
+
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+
+    this.setData({
+
+      index: e.detail.value
+
+    })
+
+  },
+
+  //日期选择
+
+  bindDateChange: function (e) {
+
+    this.setData({
+
+      date: e.detail.value
+
+    })
+
+  },
+
+  //时间选择
+
+  bindTimeChange: function (e) {
+
+    this.setData({
+
+      time: e.detail.value
+
+    })
+
+  },
+
+
+
+})
