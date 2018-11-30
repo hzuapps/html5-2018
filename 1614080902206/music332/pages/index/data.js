3b624b3842e67988b4bd6b695c3ceeccf1699fb7
HRChen <253933434.com>
2018-11-30 19:36:48 +0800

    #7 第七次实验

---
+++ 1614080902206/music332/pages/index/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: 'http://zhangmenshiting.qianqian.com/data2/music/293c57cbd8418bed06a1ef5da4e2d436/604632797/604632797.m4a?xcode=965a6984a796de080d9b8f0c677c76d2',
+    poster: 'http://p2.music.126.net/5M7Qvv32HaFmMrLN8uehuA==/109951163293243877.jpg?imageView&thumbnail=360y360&quality=75&tostatic=0',
+    name: '去年夏天',
+    author: ''
+  },
+  {
+    src: 'http://zhangmenshiting.qianqian.com/data2/music/a1ef4dd63eccd45544c5844e5449ae3c/604568283/604568283.m4a?xcode=259eecca4ac3b30d48ae5c20176c0886',
+    poster: 'http://himg.bdimg.com/sys/portrait/item/4471363736373336378f01.jpg',
+    name: '往后余生',
+    author: ''
+  },
+  {
+    src: 'http://zhangmenshiting.qianqian.com/data2/music/9be7ef0fee5454a9048fb0d84e352630/601427401/601427401.m4a?xcode=259eecca4ac3b30d21ec55f2ef944ac8',
+    poster: 'http://qukufile2.qianqian.com/data2/pic/8d356491f24692ff802cc49c80f51fee/601427385/601427385.jpg@s_1,w_300,h_300',
+    name: '卡路里',
+    author: ''
+  },
+  {
+    src: 'http://zhangmenshiting.qianqian.com/data2/music/115e998370f37bd06b1d88596dd0db08/594707960/594707960.m4a?xcode=6a3c8f9c4a4393a9e9c5304b21fec782',
+    poster: 'http://qukufile2.qianqian.com/data2/pic/b39fe4193fe2a5b3991e6c10f5709d0a/547659457/547659457.jpg@s_0,w_300',
+    name: 'share of you',
+    author: ''
+  }
+]
