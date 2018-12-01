231c7b0a1817897112e7cdb98bd3d9a3e9f85e16
li763407418 <763407418@qq.com>
2018-12-02 02:45:27 +0800

    第七次实验

---
+++ 1614080902325/游戏资讯/pages/ss/ss.js
@@ -0,0 +1,26 @@
+// pages/home/ss.js
+function getRandomColor() {
+  let rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
+Page({
+  onReady(res) {
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+  inputValue: '',
+  bindInputBlur(e) {
+    this.inputValue = e.detail.value
+  },
+  bindSendDanmu() {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  }
+})
