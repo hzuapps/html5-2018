e37650d4ec56de2f11714893974951e00d3550cd
SuevivorC <dmsz1601@163.com>
2018-12-01 20:56:18 +0800

    #7 实验七

---
+++ 1614080902406/app5/pages/input/input.js
@@ -0,0 +1,16 @@
+Page({
+    data: {
+        showTopTips: false,
+
+        radioItems: [
+            {name: 'cell standard', value: '0'},
+            {name: 'cell standard', value: '1', checked: true}
+        ],
+        checkboxItems: [
+            {name: 'standard is dealt for u.', value: '0', checked: true},
+            {name: 'standard is dealicient for u.', value: '1'}
+        ],
+
+        isAgree: false
+    }
+});
