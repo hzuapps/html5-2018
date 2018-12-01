a45316914543d32327e9539238b6f745052e7552
1614080902239 <37758663+1614080902239@users.noreply.github.com>
2018-12-02 01:37:13 +0800

    Add files via upload

--- 1614080902239/lpy/pages/pfirst/pfirst.js
+++ 1614080902239/lpy/pages/pfirst/pfirst.js
@@ -1,66 +1,103 @@
-// pages/pfirst/pfirst.js
+//pages/navigator / navigator.js
 Page({
-
   /**
-   * 页面的初始数据
-   */
+  * 页面的初始数据
+  */
   data: {
-
+    date: '',
+    name: ''
   },
-
+  // 时间段选择 
+  //显示时间表
+  bindDateChange(e) {
+    console.log(e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindNameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  indexBtn: function () {
+    if (this.data.name && this.data.date) {
+      wx.switchTab({
+        url: '../logs/logs',
+      })
+      wx.setStorage({
+        key: 'addName',
+        data: this.data.name,
+      })
+      wx.setStorage({
+        key: 'addDate',
+        data: this.data.date,
+      })
+    } else {
+      wx.showModal({
+        title: '书目名为空',
+        content: '书目名不能为空',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('确定添加')
+          }
+        }
+      })
+    }
+  },
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * 生命周期函数--监听页面加载
+  */
   onLoad: function (options) {
 
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  * 生命周期函数--监听页面初次渲染完成
+  */
   onReady: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面显示
-   */
+  * 生命周期函数--监听页面显示
+  */
   onShow: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
-   */
+  * 生命周期函数--监听页面隐藏
+  */
   onHide: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面卸载
-   */
+  * 生命周期函数--监听页面卸载
+  */
   onUnload: function () {
 
   },
 
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  * 页面相关事件处理函数--监听用户下拉动作
+  */
   onPullDownRefresh: function () {
 
   },
 
   /**
-   * 页面上拉触底事件的处理函数
-   */
+  * 页面上拉触底事件的处理函数
+  */
   onReachBottom: function () {
 
   },
 
   /**
-   * 用户点击右上角分享
-   */
+  * 用户点击右上角分享
+  */
   onShareAppMessage: function () {
 
   }
-})
+}) 
