29a709b4d45b4253755e141bf518b6d5512efd52
zelstudy <37758509+zelstudy@users.noreply.github.com>
2018-12-02 00:49:36 +0800

    第七次实验修改

--- 1614080902120/myproject/pages/person/person.js
+++ 1614080902120/myproject/pages/person/person.js
@@ -29,7 +29,32 @@ Page({
     })
 
   },
+  // 上传图片接口
+  doUpload: function () {
+    var that = this
 
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        var filePath = res.tempFilePaths[0]
+        // 上传图片
+        that.setData({
+          imgUrl: filePath
+        })
+      }
+    })
+  },
+
+  // 预览图片
+  previewImg: function () {
+    wx.previewImage({
+      current: this.data.imgUrl,
+      urls: [this.data.imgUrl]
+    })
+  },
   bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
