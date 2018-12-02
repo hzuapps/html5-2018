c7ea0bcd589ceb866bd69a5b62cbb6eb2560d96c
wuyut <1349939541@qq.com>
2018-12-02 10:23:32 +0800

    #7 实验七

--- 1606070302124/The sea of books/pages/search/search.js
+++ 1606070302124/The sea of books/pages/search/search.js
@@ -1,7 +1,66 @@
 // pages/search/search.js
-var initData = '\n《哲学的故事》\n\n《西方的智慧》\n\n《黄金时代》\n\n《霍乱时期的爱情》\n\n《美的历史》\n\n《天体运行论》\n';
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    text: initData
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
