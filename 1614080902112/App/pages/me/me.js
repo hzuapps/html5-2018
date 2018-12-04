9165e1f50092c42dcf173ca3bc41614b82109d4c
zhengzekun <37758532+zhengzekun@users.noreply.github.com>
2018-12-04 21:02:24 +0800

    #738  #7  第七次试验（老师看这个，前面我有个传错文件夹了）

--- 1614080902112/App/pages/me/me.js
+++ 1614080902112/App/pages/me/me.js
@@ -1,66 +1,74 @@
 // pages/me/me.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tempFilePaths: '',
+    nickName: '',
+    userInfoAvatar: '',
+    sex: '',
+    province: '',
+    city: '',
+    items: [
+      { name: 'man', value: '男' },
+      { name: 'femail', value: '女', checked: 'true' },
+      { name: 'bm', value: '保密' }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  chooseimage: function () {
+    var _this = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        _this.setData({
+          userInfoAvatar: res.tempFilePaths
+        })
+      },
+      radioChange: function (e) {
+        console.log('radio发生change事件，携带value值为：', e.detail.value)
+      },
+    })
   },
+  onLoad: function () {
+    var that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        // success
+        that.setData({
+          nickName: res.userInfo.nickName,
+          userInfoAvatar: res.userInfo.avatarUrl,
+          province: res.userInfo.province,
+          city: res.userInfo.city
+        })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        switch (res.userInfo.gender) {
+          case 0:
+            that.setData({
+              sex: '未知'
+            })
+            break;
+          case 1:
+            that.setData({
+              sex: '男'
+            })
+            break;
+          case 2:
+            that.setData({
+              sex: '女'
+            })
+            break;
+        }
+      },
+      fail: function () {
+        // fail
+        console.log("获取失败！")
+      },
+      complete: function () {
+        // complete
+        console.log("获取用户信息完成！")
+        console.log(this.province)
+      }
+    })
   }
 })
