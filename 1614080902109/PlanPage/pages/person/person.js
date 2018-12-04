15724aea9667e9189369d0bca289d144b3daec17
GhostITS <1101862559@qq.com>
2018-12-05 00:54:16 +0800

    #7 #861 第7次实验

--- 1614080902109/PlanPage/pages/person/person.js
+++ 1614080902109/PlanPage/pages/person/person.js
@@ -1,11 +1,13 @@
 // pages/person/person.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    progress: 0,
+    disabled: false
   },
 
   /**
@@ -14,7 +16,40 @@ Page({
   onLoad: function (options) {
   
   },
-
+  
+  downLoad: function () {
+    wx.showToast({
+      title: '保存成功',
+      icon: 'success',
+      duration: 2000
+    })
+    const downloadTask = wx.downloadFile({
+      url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543951471575&di=22dd72b822d04dc9750cf31f407720e6&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180727%2Ffba62464a0d042c8aec4d7565336e05d.jpeg",
+      success: function (res) {
+        
+        console.log(res);
+        var rr = res.tempFilePath;
+        wx.saveImageToPhotosAlbum({
+          filePath: rr,
+          success(res) {
+            wx.showToast({
+              title: '已保存到相册',
+              icon: 'success',
+              duration: 2000
+            })
+            this.setData({
+              progress: 0
+            })
+          }
+        })
+      }
+    })
+    downloadTask.onProgressUpdate((res) => {
+      this.setData({
+        progress: res.progress
+      })
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
