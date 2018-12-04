7b48934f13e8f6e0a67b2a9487b50e6fec9337ad
haoy1220 <30833151+haoy1220@users.noreply.github.com>
2018-12-04 11:46:30 +0800

    #7
    实验7

--- 1614080902145/privateManager/pages/index/index.js
+++ 1614080902145/privateManager/pages/index/index.js
@@ -50,5 +50,100 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  //处理表单发送的数据
+  formSubmit: function (res) {
+    let val = res.detail.value
+    clearTimeout(this.closeTime)
+    //判断数据的合法性
+    if (!val.title) {
+      this.setData({
+        tip: "使用方不能为空",
+        tipStatus: false
+      })
+    } else if (!val.username) {
+      this.setData({
+        tip: "用户名不能为空",
+        tipStatus: false
+      })
+    } else if (!val.password) {
+      this.setData({
+        tip: "密码不能为空",
+        tipStatus: false
+      })
+    } else {
+
+      //新增的内容
+      const fileSys = wx.getFileSystemManager()
+      let json = {
+        username: val.username,
+        password: val.password,
+        tip: val.tip
+      }
+      let rJson = null
+      let wJson = null
+
+      //json本地化
+      function saveJson(transJson) {
+        wJson = JSON.stringify(transJson)
+        fileSys.writeFileSync(`${wx.env.USER_DATA_PATH}/importance.importance`, wJson, "utf8")
+      }
+
+      //成功提示
+      function successTip(strTip) {
+        wx.showToast({
+          title: strTip,
+          icon: 'success',
+          duration: 3000
+        });
+      }
+
+      //数据本地化
+      try {
+        rJson = fileSys.readFileSync(`${wx.env.USER_DATA_PATH}/importance.importance`, "utf8")
+        rJson = JSON.parse(rJson)
+        //如果rJson[val.title]已经存在，提示是否覆盖
+        if (rJson[val.title]) {
+          wx.showModal({
+            title: '覆盖信息',
+            content: '<' + val.title + '>已经备忘，是否覆盖',
+            confirmText: "是",
+            cancelText: "否",
+            success: function (res) {
+              if (res.confirm) {
+                rJson[val.title] = json
+                saveJson(rJson)
+                successTip("覆盖成功")
+              } else {
+              }
+            }
+          });
+        } else {
+          rJson[val.title] = json
+          saveJson(rJson)
+          successTip("新建成功")
+        }
+        //捕获readFileSync异常，说明没有创建importance.importance
+      } catch (e) {
+        let tJson = {}
+        tJson[val.title] = json
+        saveJson(tJson)
+        successTip("新建成功")
+      }
+
+      //成功，保存状态
+      this.setData({
+        tip: "",
+        tipStatus: true
+      })
+    }
+
+    //提示定时消失
+    this.closeTime = setTimeout(() => {
+      this.setData({
+        tip: "",
+        tipStatus: false
+      })
+    }, 2000)
   }
 })
--- 1614080902145/privateManager/pages/manage/manage.js
+++ 1614080902145/privateManager/pages/manage/manage.js
@@ -5,14 +5,105 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    json: null,
+    jsonKeyArr: null,
+    joinSumArr: null,
+    isNul: true
   },
+  del: function (event) {
+    console.log(event)
+    let tit = event.currentTarget.id
+    //删除提示
+    wx.showModal({
+      title: '删除',
+      content: '是否要删除<' + tit + '>',
+      confirmText: "删除",
+      cancelText: "取消",
+      success: (res) => {
+        if (res.confirm) {
+          let tJson = this.data.json
+          console.log("del befor")
+          console.log(tJson)
+          delete tJson[tit]//删除
+          console.log("after del")
+          console.log(tJson)
+          //本地化
+          try {
+            const fileSys = wx.getFileSystemManager()
+            let wJson = JSON.stringify(tJson)
+            fileSys.writeFileSync(`${wx.env.USER_DATA_PATH}/importance.importance`, wJson, "utf8")
+            wx.showToast({
+              title: "删除成功",
+              icon: 'success',
+              duration: 3000
+            });
+            this.initMag()
+          } catch (e) {
+            console.log(e)
+          }
+        } else {
 
+        }
+      }
+    });
+  },
+  //获取弹窗所需信息（username，password，tip）
+  getMsg: function (event) {
+    console.log(event.currentTarget.id)
+    let tit = event.currentTarget.id
+    let current = this.data.json[tit]
+    let tip = null
+    if (current.tip) {
+      tip = '备    注：' + current.tip
+    } else {
+      tip = ''
+    }
+    //弹窗提示
+    wx.showModal({
+      title: tit,
+      content: '用户名：' + current.username + '\r\n' + '密    码：' + current.password + '\r\n' + tip,
+      confirmText: "确定",
+    });
+  },
+  //初始化信息，在onLoad,onShow上执行
+  initMag() {
+    let rJson = null
+    const fileSys = wx.getFileSystemManager()
+    try {//获取数据
+      rJson = fileSys.readFileSync(`${wx.env.USER_DATA_PATH}/importance.importance`, "utf8")
+      rJson = JSON.parse(rJson)
+
+      let keyArr = Object.keys(rJson)
+      let tArr = []
+      for (let x = 0; x < keyArr.length; x++) {
+        tArr[x] = x
+      }
+      console.log(keyArr.length)
+      if (!keyArr.length) {
+        this.setData({
+          isNul: true
+        })
+      } else {
+        this.setData({
+          isNul: false
+        })
+      }
+      this.setData({
+        json: rJson,
+        jsonKeyArr: keyArr,
+        joinSumArr: tArr
+      })
+    } catch (e) {//说明没有文件
+      this.setData({
+        isNul: true
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.initMag()
   },
 
   /**
@@ -26,7 +117,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.initMag()
   },
 
   /**
