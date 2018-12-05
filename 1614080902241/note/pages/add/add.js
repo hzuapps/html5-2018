beb9bbed5cc1467e919f675392cab184ebd1d671
LuoJKL <1226848551@qq.com>
2018-12-05 17:46:46 +0800

    实验7

--- 1614080902241/note/pages/add/add.js
+++ 1614080902241/note/pages/add/add.js
@@ -1,9 +1,76 @@
 // pages/add/add.js
 Page({
+
+  data: {
+    content: ''
+  },
+
+  onLoad: function (e) {
+    var id = e.id;
+    if (id) {
+      getData(id, this);
+    } else {
+      this.setData({
+        id: Date.now()
+      })
+    }
+  },
+
+  add(e) {
+    var val = e.detail.value;
+    this.setData({
+      content: val
+    });
+  },
+
   cancel() {
+    wx.navigateBack();
+  },
+
+  submit() {
+    var reg = /^\s*$/g;
+    if (!this.data.content || reg.test(this.data.content)) {
+      console.log('不能为空');
+      return;
+    }
+    this.setData({
+      time: Date.now()
+    });
+    setValue(this);
     wx.navigateBack();
   }
 })
 
+function getData(id, page) {
+  var arr = wx.getStorageSync('txt');
+  if (arr.length) {
+    arr.forEach((item) => {
+      if (item.id == id) {
+        page.setData({
+          id: item.id,
+          content: item.content
+        })
+      }
+    })
+  }
+}
 
-
+function setValue(page) {
+  var arr = wx.getStorageSync('txt');
+  console.log(arr);
+  var data = [], flag = true;
+  if (arr.length) {
+    arr.forEach(item => {
+      if (item.id == page.data.id) {
+        item.time = Date.now();
+        item.content = page.data.content;
+        flag = false;
+      }
+      data.push(item);
+    })
+  }
+  if (flag) {
+    data.push(page.data);
+  }
+  wx.setStorageSync('txt', data);
+}
--- 1614080902241/note/pages/index/index.js
+++ 1614080902241/note/pages/index/index.js
@@ -1,7 +1,76 @@
+var util = require('../../utils/util');
+
 Page({
+
+  data: {
+    lists: []
+  },
+
+  onLoad: function (options) {
+    initData(this);
+  },
+
+
+  onShow: function () {
+    initData(this);
+  },
+
+  edit(e) {
+    var id = e.currentTarget.dataset.id;
+   wx.navigateTo({
+      url: '../add/add?id=' + id
+    })
+  },
+
+  delete(e){
+     var id = e.currentTarget.dataset.id;
+     var arr=wx.getStorageSync('txt');
+     var arrs;
+     var that=this;
+     var f=1;
+     wx.showModal({
+       title: '提示',
+       content: '确认删除吗？',
+       success: function (res) {
+         if (res.confirm) {
+           console.log(id)
+           arr.forEach((item, i) => {
+             if ((item.id) == id && f == 1) {
+               console.log(i);
+               arr.splice(i, 1);
+               wx.setStorageSync('txt', arr);
+               f = 0;
+             }
+           })
+           arrs = wx.getStorageSync('txt');
+           arrs.forEach((item, i) => {
+             var t = new Date(Number(item.time));
+             item.time = util.dateFormate(t);
+           })
+           that.setData({
+             lists: arrs
+           })
+         }
+       }
+     })
+  },
+
   add() {
     wx.navigateTo({
       url: '../add/add',
     })
   }
 })
+
+function initData(page) {
+  var arr = wx.getStorageSync('txt');
+  if (arr.length) {
+    arr.forEach((item, i) => {
+      var t = new Date(Number(item.time));
+      item.time = util.dateFormate(t);
+    })
+    page.setData({
+      lists: arr
+    })
+  }
+}
