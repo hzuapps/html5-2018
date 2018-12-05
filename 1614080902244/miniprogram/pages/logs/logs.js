d895c9b41f4d7b2ed9b14d7e6e8c97d9cee7c9ea
limzk <369629320@qq.com>
2018-12-05 23:03:48 +0800

    #7 第七次实验

--- 1614080902244/miniprogram/pages/logs/logs.js
+++ 1614080902244/miniprogram/pages/logs/logs.js
@@ -1,15 +1,8 @@
 //logs.js
-const util = require('../../utils/util.js')
-
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+add:function(options){
+wx.navigateTo({
+  url: '../add/add'
+})
+},
 })
