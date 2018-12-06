70afc4c81ce106bd1ad2fc36b2a87f2be52ffbc1
callmeliqin <1404683778@qq.com>
2018-12-07 01:02:02 +0800

    #7 实验7

--- 1614080902320/我的小程序/pages/write/write.js
+++ 1614080902320/我的小程序/pages/write/write.js
@@ -1,32 +1,78 @@
+var app = getApp()
 Page({
-  data:{
-    diary:""
+  data: {
+    content: '',
+    contentarray:[],
+    photos: '',
+    title:'',
   },
-  bindptap:function(){
+  diaryInput: function(e) {
+      let that = this
+      that.setData({
+        content: e.detail.value
+      })
+  },
+  
+  addPhoto: function () {
+    let that = this
     wx.chooseImage({
-      count: 9,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success(res) {
-        const tempFilePaths = res.tempFilePaths
-        wx.uploadFile({
-          url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
-          filePath: tempFilePaths[0],
-          name: 'file',
-          formData: {
-            'user': 'test'
-          },
-          success(res) {
-            const data = res.data
-            //do something
-          }
+      count: 1, // 可根据情况自由设置，默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths)
+        that.setData({
+          photos: tempFilePaths
         })
       }
-    })
+    });
   },
-  diaryInput:function(e){
+  save: function (event) {
+    let that = this;
+    var date = new Date();
+    // 再通过setData更改Page()里面的data，动态更新页面的数据
     this.setData({
-      diary: e.detail.value
+      title:date
+    })
+    var content = this.data.content
+    var newa=[{title:this.data.title,content:this.data.content,photo:this.data.photos}];
+    this.setData({
+      contentarray:this.data.contentarray.concat(newa)
+    })
+    
+    if (content != '') {
+      wx.setStorage({
+        key: "diaryContent",
+        data: this.data.contentarray
+      });
+    }
+    
+    wx.showToast({
+      title: '保存成功',
+      icon: 'success',
+      duration: 2000,
+      success: function (){
+        
+      }
+    });
+    setTimeout(function () {
+      wx.hideLoading();
+      wx.navigateBack();
+    }, 2000)
+  },
+  onLoad:function(){
+    var that=this
+    wx.getStorage({
+      key: 'diaryContent',
+      success: function(res) {
+        if(res.data){
+          that.setData({
+            contentarray:res.data
+          })
+        }
+      },
     })
   }
 })
