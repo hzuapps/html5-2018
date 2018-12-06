ca59cf8e3125195f2b0728dab699d753159666be
zaitao <782427829@qq.com>
2018-12-06 10:28:29 +0800

    #7 第七次实验

--- 1614080902138/惠院宿舍报修/pages/index/index.js
+++ 1614080902138/惠院宿舍报修/pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imageList: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -50,5 +51,28 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  chooseImage: function (event) {
+    var that = this;
+    wx.chooseImage({
+      count: 5, // 一次最多可以选择2张图片一起上传
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var imgeList = that.data.imageList.concat(res.tempFilePaths);
+        that.setData({
+          imageList: imgeList
+        });
+      }
+    })
+  },
+  previewImage: function (e) {
+    var that = this;
+    var dataid = e.currentTarget.dataset.id;
+    var imageList = that.data.imageList;
+    wx.previewImage({
+      current: imageList[dataid],
+      urls: this.data.imageList
+    });
   }
 })
