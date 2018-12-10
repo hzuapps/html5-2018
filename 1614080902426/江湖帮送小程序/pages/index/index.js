d55ad69c093fd13a5fdbf94e7266045bac57fea4
linzsong <744740666@qq.com>
2018-12-10 16:41:04 +0800

    #7 第七次实验

--- 1614080902426/江湖帮送小程序/pages/index/index.js
+++ 1614080902426/江湖帮送小程序/pages/index/index.js
@@ -1,54 +1,117 @@
-//index.js
-//获取应用实例
-const app = getApp()
+//引用腾讯地图API
+var QQMapWX = require('../../lib/qqmap-wx-jssdk.js');
+var qqmapsdk;
 
 Page({
+ 
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    //导航栏
+    navbar: ['我要送', '我来送'],
+    currentTab: 0,
+   
+    //map data
+    address: "",
+    src: "",
+    latitude: 0,
+    longitude: 0,
+    markers: [{
+      
+    }
+    ],
+    startLocation: {//起始位置数据
+      longitude: 0,
+      latitude: 0,
+      address: '',
+    },
+    mobileLocation: {//移动选择位置数据
+      longitude: 0,
+      latitude: 0,
+      address: '',
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  //导航栏切换页面
+  navbarTap: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      currentTab: e.currentTarget.dataset.idx,  
     })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'MYKBZ-IVMWI-ZBRGQ-5QKUW-LBRF7-YYBCC'
+    });
+    var that = this;
+    //获取位置
+    wx.getLocation({
+      type: 'gcj02',//默认为 wgs84 返回 gps 坐标，gcj02 返回可用于wx.openLocation的坐标
+      success: function (res) {
+        //console.log(res);
+        var marker = [{
+          id: 0,
+          latitude: res.latitude,
+          longitude: res.longitude,
+          
+        }];
+        //起始选择位置数据
+        var startLocation = {
+          latitude: res.latitude,
+          longitude: res.longitude,
+        };
+        //移动选择位置数据
+        var mobileLocation = {
+          latitude: res.latitude,
+          longitude: res.longitude,
+        };
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: marker,
+        });
+        //根据坐标获取当前位置名称，显示在顶部:腾讯地图逆地址解析
+        qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: function (addressRes) {
+            var address = addressRes.result.formatted_addresses.recommend;
+            mobileLocation.address = address;
+            console.log(address)
+            //当前位置信息
+            that.setData({
+              mobileLocation: mobileLocation,
+            });
+            that.setData({
+              startLocation: startLocation,
+            });
+          }
+        });
+      }
+    });
+
+    this.mapCtx = wx.createMapContext('myMap');
+    
+  },
+  //移动选点
+  moveToLocation: function () {
+    var that = this;
+    wx.chooseLocation({
+      success: function (res) {
+        let mobileLocation = {
+          
+          address: res.address,
+        };
+        that.setData({
+          mobileLocation: mobileLocation,
+        });
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    });
   }
+
 })
