4578c38a406636629c257fc2f140e0a6c493ee45
xiaozzcc <1360559183@qq.com>
2018-12-17 13:15:36 +0800

    实验七

--- 1614080902415/timer/pages/setting/setting.js
+++ 1614080902415/timer/pages/setting/setting.js
@@ -4,20 +4,48 @@ Page({
       title: '设置'
     })
     this.setData({
-    	workTime: wx.getStorageSync('workTime'),
-    	restTime: wx.getStorageSync('restTime')
+      workTime: wx.getStorageSync('workTime'),
+      restTime: wx.getStorageSync('restTime')
     })
   },
   changeWorkTime: function(e) {
-  	wx.setStorage({
-  		key: 'workTime',
-  		data: e.detail.value
-  	})
+    wx.setStorage({
+      key: 'workTime',
+      data: e.detail.value
+    })
   },
   changeRestTime: function(e) {
-  	wx.setStorage({
-  		key: 'restTime',
-  		data: e.detail.value
-  	})
+    wx.setStorage({
+      key: 'restTime',
+      data: e.detail.value
+    })
+  },
+  bindButtonTap: function(e) {
+
+    var me = this;
+    var formObject = e.detail.value;
+    var say = formObject.say;
+
+    wx.request({
+      url: "http://127.0.0.1:8080/demo/get",
+      method: "get",
+      data: {
+        say: say
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+
+        wx.showToast({
+          title: 'suceess',
+          icon: 'success',
+          duration: 2000
+        });
+      }
+
+    })
   }
+
+
 })
